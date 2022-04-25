35e8ec4b68a4bc92beb621a95990e4e6c3899999
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-04-25 16:07:26 -0400

    Add image link field to Song model + Add "img_link" field to serializer

--- songs/models.py
+++ songs/models.py
@@ -7,6 +7,7 @@ class Song(models.Model):
     release_date = models.DateField()
     genre = models.CharField(max_length=255)
     num_likes = models.PositiveIntegerField(default=0)
+    img_link = models.CharField(max_length=255, default="none")
 
     def __str__(self):
         return f"{self.artist}- {self.title}"
