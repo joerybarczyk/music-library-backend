9aba81952b5b690e8e78a20d5c31c6e10df2428f
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-04-25 15:54:13 -0400

    Add functionality to "like" or "dislike" songs through API endpoint + Convert SongDetail view to a GenericAPIView with mixins + Add patch method to SongDetail view with "action" query parameter + Add num_likes field to Song models + Add num_likes field to Song serializer

--- songs/views.py
+++ songs/views.py
@@ -1,11 +1,43 @@
+from django.shortcuts import get_object_or_404
 from .models import Song
 from .serializers import SongSerializer
-from rest_framework import generics
+from rest_framework import generics, mixins
+from rest_framework.response import Response
 
 class SongList(generics.ListCreateAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
 
-class SongDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset= Song.objects.all()
+class SongDetail(generics.GenericAPIView,
+                mixins.RetrieveModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.DestroyModelMixin):
+
+    queryset = Song.objects.all()
     serializer_class = SongSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+
+    def patch(self, request, pk):
+        song = get_object_or_404(Song, pk=pk)
+        action = self.request.query_params.get('action')
+
+        if action == "like":
+            serializer = SongSerializer(song, data={'num_likes': song.num_likes + 1}, partial=True)
+        elif action == "dislike":
+            serializer = SongSerializer(song, data={'num_likes': song.num_likes - 1}, partial=True)
+        else:
+            serializer = SongSerializer(song, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+            
+
